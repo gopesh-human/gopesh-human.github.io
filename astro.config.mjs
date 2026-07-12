aaa53f21b32de9a1305638f52045c782148d0dd8
gopesh-human <gopesh.sharma.human@gmail.com>
2026-07-12 23:25:49 +0530

    Tried some new design ideas

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from "astro/config";
 
+import icons from 'astro-icons';
+
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-    'site': 'gopesh-human.github.io',
+  'site': 'https://gopesh-human.github.io',
+  integrations: [icons(), icon()],
 });
